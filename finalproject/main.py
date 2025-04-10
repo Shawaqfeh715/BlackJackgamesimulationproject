e81edf2f06bd9ac37577c6757278b379f560224d
Shawaqfeh715 <Anasshaw@umich.edu>
2025-04-10 09:37:17 -0400

    commit 4

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     cards = {"Jack": 10, "King": 10, "Queen": 10,
              "10": 10, "9": 9, "8": 8, "7": 7, "6": 6,
@@ -16,6 +18,17 @@ class Card:
         self.value_of_card = self._get_type_of_card(result)
         return self.value_of_card
 
+class Deck:
+
+      def __int__(self):
+          self.deck=[]
+
+      def make_deck(self):
+          card=Card()
+          card.type_of_card=random.randint(1,10)
+          for i in range(53):
+              self.deck.append(card)
+
 class Shoe:
     def __init__(self, number_of_decks=6):
             self.number_of_decks = number_of_decks
@@ -30,4 +43,11 @@ class Shoe:
                 for rank in ranks:
                     for suit in suits:
                         self.cards.append(Card(rank, suit))
+class Hand:
+
+      def __init__(self):
+          self.number_of_cards=2
+          self.cards_in_hand=[]
+
+
 
